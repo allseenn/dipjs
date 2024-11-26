27e3b247d2b7444779b95bbaf58d5457f7b77912
allseenn <allseen@live.ru>
2024-11-26 14:13:18 +0000

    0

--- bsec/bsec.c
+++ bsec/bsec.c
@@ -113,6 +113,13 @@ void output_ready(int64_t timestamp, float iaq, uint8_t iaq_accuracy,
             temperature, raw_temperature, humidity, raw_humidity,
             pressure / 100 * hectoPascal, gas/1000, co2_equivalent,
             breath_voc_equivalent, iaq, static_iaq, iaq_accuracy, bsec_status);
+
+    // Находим длину строки
+    int len = strlen(buffer);
+
+    // Устанавливаем нулевой символ сразу после последнего значимого символа
+    buffer[len] = '0';
+
     sprintf(command, "redis-cli set %lu '%s'", (unsigned long)t, buffer);
     system(command);
 
