d4955ebce8f902242a13ee0c18ba504df3267623
allseenn <allseen@live.ru>
2024-11-28 14:42:18 +0000

    react

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Grid, Typography, Paper } from '@mui/material';
-import './index.css'; // Подключаем стили
+import './index.css'; 
 
 const App = () => {
     const [metrics, setMetrics] = useState({});
@@ -10,13 +10,12 @@ const App = () => {
         { id: 'weatherHum', title: 'Влажность на улице', unit: '%', blue: [61, 100], green: [31, 60], red: [0, 30]  },
     ];
 
-    // Функция для получения данных с API
     const fetchData = async () => {
         try {
             const response = await fetch('/api/data');
             if (response.ok) {
                 const data = await response.json();
-                console.log('Fetched data:', data); // Логирование для дебага
+                console.log('Fetched data:', data); 
                 setMetrics((prevMetrics) => ({
                     ...prevMetrics,
                     ...data,
@@ -29,16 +28,14 @@ const App = () => {
         }
     };
 
-    // Функция для получения данных о погоде
+   
     const fetchWeather = async () => {
         try {
             const response = await fetch('https://wttr.in/Moscow?format=%t+%h');
             const weather = await response.text();
-            console.log('Fetched weather:', weather); // Логирование для дебага
 
             const weatherData = weather.split(' ');
             
-            // Проверяем, что в ответе действительно есть два значения
             if (weatherData.length === 2) {
                 const newWeather = {
                     weatherTemp: parseFloat(weatherData[0].replace('°C', '')),
@@ -58,29 +55,26 @@ const App = () => {
     };
 
     useEffect(() => {
-        // Запрос данных при монтировании компонента
         fetchData();
         fetchWeather();
 
-        // Устанавливаем интервал для обновления данных
         const interval = setInterval(() => {
             fetchData();
             fetchWeather();
         }, 3000);
 
-        // Очищаем интервал при размонтировании компонента
         return () => clearInterval(interval);
     }, []);
 
     const cards = [
-        { id: 'temp', title: 'Температура воздуха', unit: '°C', blue: [0, 10], green: [11, 20], red: [21, 40] },
-        { id: 'raw_temp', title: 'Некомпенсированная температура воздуха', unit: '°C', blue: [0, 10], green: [11, 20], red: [21, 40] },
+        { id: 'temp', title: 'Температура воздуха', unit: '°C', blue: [0, 22], green: [23, 26], red: [27, 40] },
+        { id: 'raw_temp', title: 'Некомпенсированная температура воздуха', unit: '°C', blue: [0, 22], green: [23, 26], red: [27, 40] },
         { id: 'humidity', title: 'Относительная влажность воздуха', unit: '%', blue: [61, 100], green: [31, 60], red: [0, 30] },
         { id: 'raw_hum', title: 'Некомпенсированная влажность воздуха', unit: '%', blue: [61, 100], green: [31, 60], red: [0, 30] },
         { id: 'press', title: 'Атмосферное давление', unit: 'mmHg', blue: [600, 720], green: [721, 770], red: [771, 800]  },
         { id: 'gas', title: 'Электрическое сопротивление воздуха', unit: 'KΩ', blue: [0, 10], green: [11, 50], red: [51, 100] },
         { id: 'ecCO2', title: 'Эквивалентная концентрация CO₂', unit: 'ppm', blue: [0, 200], green: [201, 600], red: [601, 5000] },
-        { id: 'bVOC', title: 'Концентрация ЛОВ', unit: 'ppm', blue: [0, 0.5], green: [0.6, 1.5], red: [1.6, 3.0] },
+        { id: 'bVOC', title: 'Концентрация выдыхаемых ЛОВ', unit: 'ppm', blue: [0, 0.59], green: [0.6, 1.5], red: [1.6, 3.0] },
         { id: 'IAQ', title: 'Динамический индекс качества воздуха', unit: 'D-IAQ', blue: [101, 200], green: [0, 100], red: [201, 500] },
         { id: 'SIAQ', title: 'Статический индекс качества воздуха', unit: 'S-IAQ', blue: [101, 200], green: [0, 100], red: [201, 500] },
         { id: 'IAQ_ACC', title: 'Точность индекса качества воздуха', unit: 'QoS', blue: [1, 2], green: [3, 5], red: [0, 0] },
@@ -89,12 +83,11 @@ const App = () => {
         { id: 'rad_stat', title: 'Статический уровень радиации', unit: 'μR/h', blue: [30, 50], green: [0, 29], red: [51, 500] },
     ];
 
-    // Функция для получения цвета по значению
     const getColor = (value, card) => {
         if (value >= card.blue[0] && value <= card.blue[1]) return 'blue';
         if (value >= card.green[0] && value <= card.green[1]) return 'green';
         if (value >= card.red[0] && value <= card.red[1]) return 'red';
-        return 'black'; // если значение вне диапазона, возвращаем черный
+        return 'black'; 
     };
 
     return (
